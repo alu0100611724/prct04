684a86e31f7d93956566252551b43d642d8c115f
Cabaleiro <cabaleiro8@gmail.com>
2013-10-09 13:47:03 +0100

    Main.rb Versión Final

--- main.rb
+++ main.rb
@@ -1,13 +1,23 @@
 #! /usr/bin/env ruby
 
 require './fichero.rb'
+require './matriz.rb'
 
 def mostrarMatriz a
   for i in 0...a.length do
-    puts a[i]
+    puts "#{a[i]}"
   end
 end
 
 a, b = leerMatrices
 
+resultado = Array.new
+
+puts "El resultado de la suma es \n"
+resultado = sum_matrices(a, b)
+mostrarMatriz resultado
+
+puts "El resultado de la multiplicación es \n"
+resultado = mul_matrices(a, b)
+mostrarMatriz resultado
 
